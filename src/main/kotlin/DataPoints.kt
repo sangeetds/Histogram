de54879526ce8c20c7dfcae264d2f64527d2ae84
sangeetds <sangeetnarayands@gmail.com>
2021-01-06 06:07:16 +0530

    Review changes

--- src/main/kotlin/DataPoints.kt
+++ src/main/kotlin/DataPoints.kt
@@ -3,7 +3,7 @@ import kotlin.math.ceil
 class DataPoints(private val values: List<Long>) {
     val maxValue: Long = values.maxByOrNull { it } ?: 1
 
-    fun segregate(bucketSize: Long): MutableList<Long> {
+    fun segregate(bucketSize: Long): List<Long> {
         val buckets = MutableList(ceil(this.maxValue / bucketSize.toDouble()).toInt()) { 0L }
 
         this.values.forEach { num ->
