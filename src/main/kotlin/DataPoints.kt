db1db72e13cb0fa7f5dbe8952a12f0143ec1e093
sangeetds <sangeetnarayands@gmail.com>
2021-01-06 06:24:14 +0530

    Comments made

--- src/main/kotlin/DataPoints.kt
+++ src/main/kotlin/DataPoints.kt
@@ -1,8 +1,17 @@
 import kotlin.math.ceil
 
+/**
+ * Class for holding the numbers for which the histograms is build.
+ */
 class DataPoints(private val values: List<Long>) {
     val maxValue: Long = values.maxByOrNull { it } ?: 1
 
+    /**
+     * The function segregates each data point to it's respective bucket on the basis of the given
+     * bucket size.
+     * @param bucketSize Long,
+     * @return the interval list for which the histogram is made.
+     */
     fun segregate(bucketSize: Long): List<Long> {
         val buckets = MutableList(ceil(this.maxValue / bucketSize.toDouble()).toInt()) { 0L }
 
--- src/main/kotlin/FileReader.kt
+++ src/main/kotlin/FileReader.kt
@@ -1,6 +1,17 @@
 import java.io.File
 
 class FileReader {
+
+    /**
+     * Function to read values from the file.
+     * @param fileName String, the name of the file to be read.
+     * @param separator String, the regex against which the numbers will be split from values read.
+     *
+     * @return the data values and bucket size encapsulated in a Data Class FileValues.
+     *
+     * @throws java.io.FileNotFoundException when the file name is incorrect.
+     * @throws NullPointerException when the file is empty.
+     */
     fun readFile(fileName: String, separator: String): FileValues {
         val file = File(fileName)
         val bufferReader = file.bufferedReader()
